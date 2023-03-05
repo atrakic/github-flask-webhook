2a62826caf77b07e034e5f33f8b19e9f004ba9d2
Admir Trakic <atrakic@users.noreply.github.com>
2023-03-05 23:47:59 +0100

    Update localhostrun.sh

--- scripts/localhostrun.sh
+++ scripts/localhostrun.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
-# Tunnel your running container to the outside world.
-#
-# Include in your builds via
-# \curl -sSL https://raw.githubusercontent.com/atrakic/github-flask-webhook/main/scripts/localhostrun.sh | bash -s
+# Put a locally running HTTP, HTTPS or TLS app on the internet
 
-CONTAINER_NAME=${CONTAINER_NAME:?"Error, missing CONTAINER_NAME value of your running container"}
-CONTAINER_PORT=${CONTAINER_PORT:?"Error, missing CONTAINER_PORT value of your running container"}
+## Example usage:
+# \curl -sSL https://raw.githubusercontent.com/atrakic/github-flask-webhook/main/scripts/localhostrun.sh | PORT=8080 bash -s
+
+PORT=${PORT:?"Error, missing CONTAINER_PORT value of your running container"}
 TIMEOUT_SECONDS=${TIMEOUT_SECONDS:-''}  ## `man timeout`
 
 set -e
 
 cleanup() {
-  docker stop "$CONTAINER_NAME" || true
-  docker rm "$CONTAINER_NAME" || true
   echo 'Done!'
 }
 
 main() {
   local remote_port=80  
-  local ssh_args=( -aT -R "${remote_port}":localhost:"$CONTAINER_PORT" ssh.localhost.run -- )
+  local ssh_args=( -aT -R "${remote_port}":localhost:"$PORT" ssh.localhost.run -- )
 
   if [ -z "$TIMEOUT_SECONDS" ]; then
     ssh "${ssh_args[@]}"
